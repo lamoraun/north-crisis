bf8566c03ef29440cc87d1eab490a9c7263d58b5
Orrin19 <ofandomuno@gmail.com>
2025-09-11 00:17:44 +0700

    fixes

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
 	projectId: "north-crisis",
 	storageBucket: "north-crisis.firebasestorage.app",
 	messagingSenderId: "432138421130",
-	appId: "1:432138421130:web:a8c40f3cd3878c11e1be73"
+	appId: "1:432138421130:web:0cb6dcf0fd0146bfe1be73"
 };
 
 const app = initializeApp(firebaseConfig);
